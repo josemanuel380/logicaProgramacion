d6f4da822f3fbb2724bc8618c94e0f68ea21d445
josemanuel <toroj6231@gmail.com>
2026-04-05 16:36:33 -0400

    empezar con los ejercicios

--- src/main/java/com/example/Ejercios_00.java
+++ src/main/java/com/example/Ejercios_00.java
@@ -1,22 +1,8 @@
 package com.example;
 
 public class Ejercios_00 {
+    // https://www.java.com/es/
     public static void main(String[] args) {
-        // Ejercicio 1: Imprimir "Hola, Mundo!" en la consola
-        System.out.println("Hola, Mundo!");
-
-        // Ejercicio 2: Sumar dos números y mostrar el resultado
-        int num1 = 5;
-        int num2 = 10;
-        int suma = num1 + num2;
-        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
-
-        // Ejercicio 3: Verificar si un número es par o impar
-        int numero = 7;
-        if (numero % 2 == 0) {
-            System.out.println(numero + " es un número par.");
-        } else {
-            System.out.println(numero + " es un número impar.");
-        }
+    
     }
 }
