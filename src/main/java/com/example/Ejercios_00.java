3d34e42b5e07da895f630497e79f137d85afe271
josemanuel <toroj6231@gmail.com>
2026-04-05 16:47:53 -0400

    ejercicio 00 terminado

--- src/main/java/com/example/Ejercios_00.java
+++ src/main/java/com/example/Ejercios_00.java
@@ -2,7 +2,24 @@ package com.example;
 
 public class Ejercios_00 {
     // https://www.java.com/es/
+
+    /*esto es un
+    comentario de
+    varis lineas */
+
     public static void main(String[] args) {
+
+        String nombre = "Juan"; // variable de tipo String es un tipo de dato para almacenar texto
+        int edad =
+        30; // variable de tipo int es un tipo de dato primitivo para almacenar números enteros
+        double altura = 1.75; // variable de tipo double es un tipo de dato primitivo para almacenar números decimales
+        boolean esEstudiante = true; // variable de tipo boolean es un tipo de dato primitivo que puede ser verdadero o falso
+        final String PAIS = "España"; // variable constante final es un tipo de dato que no puede cambiar su valor una vez asignado
+        long telefono = 1234567890L; // variable de tipo long es un tipo de dato primitivo para almacenar números enteros grandes
+        char genero = 'M'; // variable de tipo char es un tipo de dato primitivo que representa un solo carácter
+
+        System.out.println("Hola, Java");
+
     
     }
 }
